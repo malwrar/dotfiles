6fe05558a0d43a9a4af054243118efc014ed57a6
malwrar <malwrar@gmail.com>
2020-05-09 16:03:07 -0700

    Initial commit.

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+LOCAL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+HOSTS="$LOCAL_DIR/hosts"
+PLAYBOOK="$LOCAL_DIR/dotfiles.yml"
+
+# Runs Ansible playbook using our user.
+ansible-playbook -i "$HOSTS" "$PLAYBOOK" --ask-become-pass
+
+exit 0
