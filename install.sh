f691059539f84064add918fc346ad82a749aebb3
malwrar <malwrar@gmail.com>
2020-05-24 11:40:12 -0700

    Disable BECOME prompt, nothing needs root yet.

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ HOSTS="$LOCAL_DIR/hosts"
 PLAYBOOK="$LOCAL_DIR/dotfiles.yml"
 
 # Runs Ansible playbook using our user.
-ansible-playbook -i "$HOSTS" "$PLAYBOOK" --ask-become-pass
+#ansible-playbook -i "$HOSTS" "$PLAYBOOK" --ask-become-pass
+ansible-playbook -i "$HOSTS" "$PLAYBOOK"
 
 exit 0
